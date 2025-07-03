f3d2710e87958dec468753b455503f228cce639d
aryan29gupta <aryangupta2906@gmail.com>
2025-07-04 00:38:16 +0530

    Project

--- CounterProject/src/App.jsx
+++ CounterProject/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   //for updating the value of counter in the UI 
@@ -38,3 +37,4 @@ function App() {
 }
 
 export default App
+ 
